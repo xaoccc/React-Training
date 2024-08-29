07882e2f8f9345d8c4aaf3c2ff2007b9beefccd6
xaoccc <xaocccc@gmail.com>
2024-08-29 10:06:27 +0300

    added routing

--- demo3/src/main.jsx
+++ demo3/src/main.jsx
@@ -1,6 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from '../components/about.jsx'
+import Home from '../components/home.jsx'
 import Services from '../components/services.jsx'
 import WhyUs from '../components/whyus.jsx'
 import Team from '../components/team.jsx'
@@ -11,14 +12,19 @@ import Footer from '../components/footer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
+    <BrowserRouter>
+        
         <Header />
-        <Services />
-        <About />
-        <WhyUs />
-        <Team />
-        <Clients />
+        <Routes>
+            <Route path="/React-Training" element={<Home />} />
+            <Route path="/React-Training/about" element={<About />} />            
+            <Route path="/React-Training/whyus" element={<WhyUs />} />            
+            <Route path="/React-Training/team" element={<Team />} />            
+            <Route path="/React-Training/clients" element={<Clients />} />           
+        
+            <Route path="/React-Training/services" element={<Services />} />            
+        </Routes>
         <Info />
         <Footer />
-    </StrictMode>,
+    </BrowserRouter>,
 )

dae1266bb7d58da631fba8211c48a4f6d4f29365
xaoccc <xaocccc@gmail.com>
2024-08-30 17:06:43 +0300

    demo9 update

--- demo9/components/main.jsx
+++ demo9/components/main.jsx
@@ -1,6 +1,18 @@
 import ListGroup from 'react-bootstrap/ListGroup';
+import { useState, useEffect } from 'react';
 
 export default function Main() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:3030/jsonstore/todos')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+        }, []);
+    
+
     return (
         
         <ListGroup className='w-25 mx-auto'>
